a78e566ee1b4b97ea634460b50bc5b95e2d04074
shgao <shgao@shgao-LoadGen3.(none)>
2015-10-13 14:32:27 +0800

    finish

--- bob_downloader.py
+++ bob_downloader.py
@@ -9,30 +9,34 @@ import sys
 import time
 import os
 
+BRANCH_MAP = { 
+               '11.1.1': ['kili-tip','kili'],
+               '11.3.1' : ['k2-tip','k2'],
+               '11.2.0' : ['11.2.0','k2'],
+               '11.3.0' : ['11.3.0','k2'],
+               '11.1.0' : ['11.1.0','kili'],
+               '11.4.0' : ['kanga-tip','kanga'],
+               '10.7.3' : ['jaytip','jay'],
+               '10.7.2' : ['10.7.2','jay'],
+               '10.7.2' : ['10.7.1','jay'],
+               '10.7.0' : ['10.7.0','jay']
+               # '10.6.6' : ['indextip', 'index'],
+               # '10.6.5' : ['10.6.5','index'],
+               # '10.6.4' : ['10.6.4','index'],
+               # '10.6.3' : ['10.6.3','index'],
+               # '10.6.2' : ['10.6.2','index']
+            }
+
+
 class BobDownloader(object):
     """docstring for BobDownloader"""
     _base_path = 'http://10.4.16.60/nas-bob/daily/appliance/daily/ex-{0}-appliance/{1}-{2}/{1}-{2}.iso'
+    
 
-    _branch_map = { 
-                    '11.1.1': ['kili-tip','kili'],
-                    '11.3.1' : ['k2-tip','k2'],
-                    '11.2.0' : ['11.2.0','k2'],
-                    '11.3.0' : ['11.3.0','k2'],
-                    '11.1.0' : ['11.1.0','kili'],
-                    '11.4.0' : ['kanga-tip','kanga'],
-                    '10.7.3' : ['jaytip','jay'],
-                    '10.7.2' : ['10.7.2','jay'],
-                    '10.7.2' : ['10.7.1','jay'],
-                    '10.7.0' : ['10.7.0','jay'],
-                    '10.6.6' : ['indextip', 'index'],
-                    '10.6.5' : ['10.6.5','index'],
-                    '10.6.4' : ['10.6.4','index'],
-                    '10.6.3' : ['10.6.3','index'],
-                    '10.6.2' : ['10.6.2','index']
-                 }
 
-    def __init__(self, branch, build):
+    def __init__(self, branch, build, _image_dir=None):
         super(BobDownloader, self).__init__()
+        self._image_dir = _image_dir
         self.branch = branch
         self.build = build
         self.file_name = '{0}.{1}.iso'.format(self.branch, self.build)
@@ -42,35 +46,47 @@ class BobDownloader(object):
             self.file_name = 'unstripped-' + self.file_name
             self.md5_file_name = 'unstripped-' + self.md5_file_name
         
-        self.final_path = self._base_path.format(self._branch_map[self.branch][0], 
+        self.final_path = self._base_path.format(BRANCH_MAP[self.branch][0], 
                                             self.branch, self.build)
+
+        if (self._image_dir is not None and 
+                isinstance(self._image_dir,str) and 
+                self._image_dir[-1] != '/'):
+            self._image_dir += '/'
+
+        if self._image_dir is not None and isinstance(self._image_dir, str):
+            self._full_path_file_name = self._image_dir + self.file_name
+            self._full_path_md5_file_name = self._image_dir + self.md5_file_name
+
         print(self.final_path)
 
     def start(self):
-        if self._check_exist() or !self._check_valid():
-            return
+        if self._check_exist() or (not self._check_valid()):
+            return self._full_path_file_name
 
         try:
             print("Start downloading: {0}".format(self.file_name))
             local_filename, headers = urllib.request.urlretrieve(self.final_path, 
-                            filename=self.file_name, reporthook=self._progress_hook)
+                            filename=self._full_path_file_name, reporthook=self._progress_hook)
             print("\nFinish: {0}".format(self.file_name))
             print("Start downloading: {0}".format(self.md5_file_name))
             local_filename, headers = urllib.request.urlretrieve(self.final_path, 
-                            filename=self.md5_file_name, reporthook=self._progress_hook)
+                            filename=self._full_path_md5_file_name, reporthook=self._progress_hook)
             print("\nFinish: {0}".format(self.md5_file_name))
         except urllib.error.HTTPError as e:
             print(e.code)
             print(e.read())
 
+        return self._full_path_file_name
+
     def _check_exist(self):
-        if os.path.exists(self.file_name) and os.path.exists(self.md5_file_name) and self._md5_check():
+        if os.path.exists(self._full_path_file_name) and os.path.exists(self._full_path_md5_file_name) and self._md5_check():
             print("{0} existed, skip downloading".format(self.file_name))
             return True
         return False
 
     def _check_valid(self):
-        if self.branch in self._branch_map.keys():
+        if self.branch in BRANCH_MAP.keys():
             return True
         print("{0} is invalid branch".format(self.branch))
         return False
@@ -79,17 +95,14 @@ class BobDownloader(object):
 
     def _progress_hook(self, block, block_size, total_size):
         percent = (block * block_size)*100 / total_size
-        sys.stdout.write('\b\b\b\b\b\b')
+        sys.stdout.write('\b\b\b\b\b\b\b')
         sys.stdout.write('%.2f%%' % percent)
         sys.stdout.flush()
 
 
 def run():
-    b = BobDownloader('10.7.3','217')
+    b = BobDownloader('10.7.3','217','/home/shgao/bob_build')
     b.start()
-
-def usage():
-    pass
 
 if __name__ == '__main__':
     run()
---
+++ vm_creator.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# Created Time: 2015-10-12
+
+__author__ = 'Matthew Gao'
+
+BASE_PATH = '/home/shgao/vm-imaging/vm-imaging/Firmware/EXSeriesVPN/'
+
+import getopt
+import sys
+import os
+from bob_downloader import BobDownloader, BRANCH_MAP
+
+def error_hander(func):
+	def wrapper(*args, **kargs):
+		try:
+			func(*args, **kargs)
+		except CommanderRunnerExcpetion as e:
+			print(e)
+			usage()
+			sys.exit(2)
+		except getopt.GetoptError as err:
+			print(err) # will print something like "option -a not recognized"
+			usage()
+			sys.exit(2)
+
+	return wrapper
+
+class CommanderRunnerExcpetion(Exception):
+	def __init__(self, err_str):
+		super(CommanderRunnerExcpetion, self).__init__()
+		self.err_str = err_str
+
+	def __str__(self):
+		return "CommanderRunner Error: {0}".format(self.err_str)
+
+class CommandRunner(object):
+	"""docstring for CommandRunner"""
+
+	def __init__(self):
+		super(CommandRunner, self).__init__()
+		self.internal_ip = None
+		self.build = None
+		self.appl_name = None
+
+	@error_hander
+	def opts_parser(self):
+	    opts, args = getopt.getopt(sys.argv[1:], "h:b:n:", ["host=","help","build=","name="])
+
+	    output = None
+	    verbose = False
+	    for o, a in opts:
+	        if o in ("-h", "--host"):
+	            self.internal_ip = a
+	        elif o in ('-b', '--build'):
+	        	self.build = a
+	        elif o in ('-n', '--name'):
+	        	self.appl_name = a
+	        elif o in ("--help"):
+	            usage()
+	            sys.exit()
+
+	    if self.internal_ip is None:
+	    	raise CommanderRunnerExcpetion("You have to specify a internla IP")
+	    if self.build is None:
+	    	raise CommanderRunnerExcpetion("You have to specify a bob build version")
+	    if self.appl_name is None:
+	    	raise CommanderRunnerExcpetion("You have to specify a appliance name")
+
+
+	def __call__(self):
+		self.opts_parser()
+		ver_str, build_str = self.build.split('-')
+		b = BobDownloader(ver_str,build_str,'/home/shgao/bob_build')
+		_full_iso_path = b.start()
+		print(ver_str,build_str)
+		_path = BASE_PATH + BRANCH_MAP[ver_str][1] + '/vm-imaging/'
+		os.chdir(_path)
+		#print(os.getcwd())
+		shell_str = './make-machine.sh -n {0} -i {1} -h {2}'.format(
+							self.appl_name + '-' + self.internal_ip.replace('.','-'), 
+		 					_full_iso_path, self.internal_ip)
+		print(shell_str)
+		os.system(shell_str)
+		print("ALL DONE")
+	
+
+def usage():
+	usage_str = 'Usage:\n'
+	usage_str += '\t-h [IP]|--host=[IP]: Specify the internal IP address of the appliance \n'
+	usage_str += '\t-b [ISO name]|--build=[ISO name]: Specify the version of the ISO(NOT unstripped prefix) \n'
+	usage_str += '\t-n [appliance name]|--build=[appliance name]: Specify the appliance name \n'
+	print(usage_str)
+
+if __name__ == "__main__":
+    CommandRunner()()
